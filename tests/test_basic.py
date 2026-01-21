51baa7a2dbbd1217665de08bf00706327935fde3
JesusSanchezR7 <jesus.ss.12354@gmail.com>
2026-01-21 13:43:57 -0700

    Corrige import para pytest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
 
 def test_ruta_principal():
     cliente = app.test_client()
     respuesta = cliente.get('/')
 
     assert respuesta.status_code == 200
-    assert b'Mi pipeline de DevOps funciona correctamente' in respuesta.data
